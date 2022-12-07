cdff90f9ceb97d1dca15a7bc230cbf82d6a8f61e
AlmostIvanSidorov <vanosidorov@gmail.com>
2022-12-07 12:23:34 +0200

    upd2

--- hello.py
+++ hello.py
@@ -1,4 +1,32 @@
 #!/usr/bin/python3
 
-for i in range(3):
-    print("hello")
+from testflows.core import *
+
+
+@TestScenario
+def not_hello(self):
+    with Given("Not Hello"):
+        assert("hello" == "world")
+
+
+@TestScenario
+def hello(self):
+    with Given("Hello"):
+        for i in range(3):
+            assert("hello" !=  "world");
+
+@TestModule
+def regression(self):
+    Scenario(run=not_hello)
+    Scenario(run=hello)
+    Scenario(run=not_hello)
+
+
+def main():
+   regression()
+
+
+if __name__ == '__main__':
+    main()
+
+
