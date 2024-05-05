e78a80bac0a90aabd9fadb2d69439eb12903a323
ivan <vanosidorov@gmail.com>
2024-05-05 09:16:57 +0300

    changes

--- hello.py
+++ hello.py
@@ -5,8 +5,19 @@ def main():
    your_name = input("Please enter your name:")
    return f"Hello {your_name}, glad to see you!"
 
+def extend_list(value, lst=None):
+   if lst is None:
+      lst = []
+   lst.append(value)
+   return lst
+
+def convert(n: int) -> str:
+   return ''.join(map(str,range(n+1)))
+
 if __name__ == '__main__':
-    hello_string = main()
-    print(hello_string)
+   print(convert(11))
+
+
+
 
 
