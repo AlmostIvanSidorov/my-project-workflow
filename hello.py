1240d0b1bcdf1b81dccbefc8fe260cf134d03b3d
Сидоров Иван Сергеевич <I.Sidorov@norbit.ru>
2024-05-05 12:10:30 +0300

    upd few functions

--- hello.py
+++ hello.py
@@ -14,8 +14,70 @@ def extend_list(value, lst=None):
 def convert(n: int) -> str:
    return ''.join(map(str,range(n+1)))
 
+def task_1():
+   a = ([],1)
+   a[0].append(1)
+
+   return a
+
+def task_2():
+   a = []
+   for i in range(5):
+      a.append(i)
+
+   a = {i+1:i**2 for i in range(5) if i%2 == 0}
+
+   return a 
+
+def task_3():
+   a = [1,2,3]
+   b = a
+   return a is b 
+
+import copy
+
+def task_4():
+   a = [1,[3]]
+
+   b,c = copy.copy(a),copy.deepcopy(a)
+
+   # b.append(3)
+
+   # b[1].append(4)
+
+   c.append(3)
+
+   c[1].append(4)
+
+   return a
+
+def task_5():
+   a = [i for i in range(5)]
+
+   # map
+
+   m = map(lambda x: x**2, a)
+
+   m1 = list(m)
+
+   m = map(lambda x: x**2, a)
+   
+
+   # filter
+
+   f = filter(lambda x: x%2 == 0, m)
+
+
+   # zip
+
+   z = zip(list(m1),list(f))
+
+   return list(z)
+
+
+
 if __name__ == '__main__':
-   print(convert(11))
+   print(task_5())
 
 
 
